1d2c0768cd0b604038b5d81fefa1abe25f48c39b
MosehMaguah <Mosehmaguah@gmail.com>
2023-07-31 02:07:01 +0300

    build an API

---
+++ api/v1/views/index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Index veiw for the API """
+
+from app.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+@app_views.route('/status')
+def get_status():
+    """ gets the status of the API """
+    return jsonify(status='OK')
+
+@app_views.route('/stats')
+def get_stats():
+    '''Gets number of objects for each type.
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
